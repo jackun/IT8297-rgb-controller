54dc3e34e4fabd7b9362855afeb448f0fd4afe36
jackun <jack.un@gmail.com>
2019-11-05 10:01:42 +0200

    update macos build script

--- build-macos-brew.sh
+++ build-macos-brew.sh
@@ -1,20 +1,21 @@
 #!/bin/zsh
 
 OUT=rgblights.macos
-LIBUSB_VER=1.0.23
 
-if [ ! -d /usr/local/Cellar/libusb/$LIBUSB_VER/include/libusb-1.0 ];then
-    if [ ! -f $(which brew) ]; then
-        echo "Get brew from https://brew.sh (and install libusb)"
-        exit 1
-    else
-        echo Installing libusb...
-        brew install libusb
-    fi
+if [ ! -f $(which brew) ]; then
+    echo "Get brew from https://brew.sh (and install hidapi and pkgconfig)"
+    exit 1
+fi
+
+brew list hidapi pkgconfig 2>/dev/null 1>/dev/null
+if [ $? -ne 0 ] ; then
+    echo Installing hidapi and pkgconfig...
+    # pkg-config might need opening a new shell (updated $PATH)?
+    brew install hidapi pkgconfig
 fi
 
 echo Building...
 llvm-g++ -o $OUT rgblights/rgblights.cpp Demo/Demo.cpp -I./rgblights \
-       -std=c++11 -lusb-1.0 \
-       -I/usr/local/Cellar/libusb/$LIBUSB_VER/include/libusb-1.0 && \
-       echo OK. Run it with \"./$OUT\" || echo failed
+    -std=c++11 -DHAVE_HIDAPI=1 \
+    $(pkg-config --cflags --libs hidapi) && \
+    echo OK. Run it with \"./$OUT\" || echo failed
